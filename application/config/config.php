b8cba44d1016485e9fcbddce615e5f807bfbf8de
Christian Rosandhy <tianrosandhy@gmail.com>
2019-07-12 11:11:41 +0800

    dynamic config base url

--- application/config/config.php
+++ application/config/config.php
@@ -1,8 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['base_url'] = 'http://localhost/php-simple-inventory';
-
+$root = "http://".$_SERVER['HTTP_HOST'];
+$root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+$config['base_url']    = $root;
 /*
 |--------------------------------------------------------------------------
 | Index File
